c6adf821015e63def9a4249a5a4f0842f2dfb353
Pierre Molinaro <pierre@pcmolinaro.name>
2005-02-04 16:34:56 +0000

    Ajout fichiers d'en-tete indispensables a la composition, plus gros exemple

---
+++ hand_coded_sources/AC_machineDefinition.h
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------*
+//                                                                           *
+//  Copyright (C) 1999-2002 Pierre Molinaro.                                 *
+//  e-mail : molinaro@irccyn.ec-nantes.fr                                    *
+//  IRCCyN, Institut de Recherche en Communications et Cybernétique de Nantes*
+//  ECN, Ecole Centrale de Nantes (France)                                   *
+//                                                                           *
+//  This program is free software; you can redistribute it and/or modify it  *
+//  under the terms of the GNU General Public License as published by the    *
+//  Free Software Foundation.                                                *
+//                                                                           *
+//  This program is distributed in the hope it will be useful, but WITHOUT   *
+//  ANY WARRANTY; without even the implied warranty of MERCHANDIBILITY or    *
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for *
+//   more details.                                                           *
+//                                                                           *
+//---------------------------------------------------------------------------*
+
+#include "bdd/C_bdd.h"
+
+//---------------------------------------------------------------------------*
+
+#define macro_AC_machineDefinition \
+  public : virtual void computeFromExpression (C_lexique & inLexique, \
+                                               const bool inDisplayBDDvaluesCount, \
+                                               const bool inDisplayBDDvalues, \
+                                               const TC_unique_dyn_array <C_string> & inInputNamesArray, \
+                                               const TC_unique_dyn_array <C_string> & inOutputNamesArray, \
+                                               C_bdd & outInitialStatesBDD, \
+                                               C_bdd & outAccessibleStatesBDD, \
+                                               C_bdd & outAccessibilityRelationBDD) const = 0 ;
+
+//---------------------------------------------------------------------------*
+
+#define macroInheritFrom_AC_machineDefinition \
+  public : virtual void computeFromExpression (C_lexique & inLexique, \
+                                               const bool inDisplayBDDvaluesCount, \
+                                               const bool inDisplayBDDvalues, \
+                                               const TC_unique_dyn_array <C_string> & inInputNamesArray, \
+                                               const TC_unique_dyn_array <C_string> & inOutputNamesArray, \
+                                               C_bdd & outInitialStatesBDD, \
+                                               C_bdd & outAccessibleStatesBDD, \
+                                               C_bdd & outAccessibilityRelationBDD) const ;
+
+//---------------------------------------------------------------------------*
